0d24681ada6295f22ee75d596f690b0f9472ffaf
voidcatwallker <thearinast@mail.ru>
2023-07-29 02:59:00 +0300

    config first bootstrap -> discovery first lookup

---
+++ discovery-service/src/main/java/com/bookstore/discoveryservice/config/WebSecurityConfig.java
@@ -0,0 +1,21 @@
+package com.bookstore.discoveryservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(a -> a.anyRequest().authenticated())
+            .httpBasic(Customizer.withDefaults())
+            .formLogin(Customizer.withDefaults());
+        return http.build();
+    }
+}
